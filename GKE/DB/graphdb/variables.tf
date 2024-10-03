686fd602cb938382e848b23db0b29f7fc54339fa
DimiCT <dimitri.griparis@ctengineeringgroup.com>
2024-10-03 10:40:47 +0300

    Update variables.tf
    update cluster external ip

--- GKE/DB/graphdb/variables.tf
+++ GKE/DB/graphdb/variables.tf
@@ -1,7 +1,7 @@
 variable "kubernetes_host" {
   description = "The hostname (in form of URI) of the Kubernetes API."
   type        = string
-  default     = "35.233.26.165" # Replace with the appropriate IP address or hostname
+  default     = "35.195.134.144"     //"35.233.26.165" # Replace with the appropriate IP address or hostname
 }
 
 variable "kubernetes_token" {
